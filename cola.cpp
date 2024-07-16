167ab25f12460b90714988f82b4766e7f2936a13
Yang <91367516+NaughtyJade@users.noreply.github.com>
2024-07-16 16:23:18 +0800

    Create cola.cpp

---
+++ cola.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a, sum;
+    while(cin >> a){
+        int sum = a;
+        while(true){
+            sum += a / 3;
+            if(a < 3){
+            	 break;
+           	}
+           	else{
+            	a=(a/3+a%3);
+            }
+        }
+        if(a == 2){
+            sum+=1;
+        }
+        	cout<<sum<<endl;
+    }
+}
