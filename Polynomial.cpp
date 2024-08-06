49b6fea441cae853cc8bed0f32039543c3fa5eb1
Yang <91367516+NaughtyJade@users.noreply.github.com>
2024-08-06 19:10:58 +0800

    Update Polynomial.cpp

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -5,74 +5,99 @@ using namespace std;
 
 class Polynomial;
 
-class Term{
+class Term {
     friend Polynomial;
 private:
     float coef;
     int exp;
+public:
+    Term() : coef(0), exp(0) {}
 };
 
-class Polynomial{
+class Polynomial {
 private:
     Term *termArray;
     int capacity;
     int terms;
 public:
-    Polynomial(){
+    Polynomial() {
         capacity = 2;
         termArray = new Term[capacity];
         terms = 0;
     }
+
+    // 拷貝構造函數
+    Polynomial(const Polynomial& poly) {
+        capacity = poly.capacity;
+        terms = poly.terms;
+        termArray = new Term[capacity];
+        copy(poly.termArray, poly.termArray + terms, termArray);
+    }
+
+    // 賦值運算符
+    Polynomial& operator=(const Polynomial& poly) {
+        if (this == &poly) return *this; // 自我賦值檢查
+        delete[] termArray;
+        capacity = poly.capacity;
+        terms = poly.terms;
+        termArray = new Term[capacity];
+        copy(poly.termArray, poly.termArray + terms, termArray);
+        return *this;
+    }
+
+    // 析構函數
+    ~Polynomial() {
+        delete[] termArray;
+    }
+
     //--------------------------------------------------------------
     void show(){
         int pos = terms;
         for (int i = 0; i < terms; i++) {
-            if (termArray[pos].exp == 0) {
+            if (i == terms - 1) {
                 if (termArray[pos].coef >= 0) {
-                    cout << " + " << termArray[pos].coef << endl;
+                    cout << " + " << termArray[i].coef << endl;
                 }
                 else{
-                    cout << " - " << termArray[pos].coef << endl;
+                    cout << " - " << termArray[i].coef << endl;
                 }
             }
             else{
-                if (pos == terms) {
-                    if (termArray[pos].coef >= 0) {
-                        cout << termArray[pos].coef << "X^" << termArray[pos].exp;
+                if (i + terms == terms) {
+                    if (termArray[i].coef >= 0) {
+                        cout << termArray[i].coef << "X^" << termArray[i].exp;
                     }
                     else{
-                        cout << termArray[pos].coef << "X^" << termArray[pos].exp;
+                        cout << termArray[i].coef << "X^" << termArray[i].exp;
                     }
                 }
                 else{
-                    if (termArray[pos].coef >= 0) {
-                        cout << " + " << termArray[pos].coef << "X^" << termArray[pos].exp;
+                    if (termArray[i].coef >= 0) {
+                        cout << " + " << termArray[i].coef << "X^" << termArray[i].exp;
                     }
                     else{
-                        cout << " - " << termArray[pos].coef << "X^" << termArray[pos].exp;
+                        cout << " - " << termArray[i].coef << "X^" << termArray[i].exp;
                     }
                 }
             }
         }
     }
     //--------------------------------------------------------------
-    Polynomial Add(Polynomial poly){
+    Polynomial Add(Polynomial poly) {
         Polynomial c;
         int aPos = 0, bPos = 0;
-        while((aPos < terms) && (bPos < poly.terms)){
-            if (termArray[aPos].exp == termArray[bPos].exp) {
-                float t = termArray[aPos].coef + termArray[bPos].coef;
+        while ((aPos < terms) && (bPos < poly.terms)) {
+            if (termArray[aPos].exp == poly.termArray[bPos].exp) {
+                float t = termArray[aPos].coef + poly.termArray[bPos].coef;
                 if (t) {
                     c.NewTerm(t, termArray[aPos].exp);
-                    aPos++;
-                    bPos++;
                 }
-            }
-            else if (termArray[aPos].exp < termArray[bPos].exp){
-                c.NewTerm(termArray[bPos].coef, termArray[bPos].exp);
+                aPos++;
                 bPos++;
-            }
-            else{
+            } else if (termArray[aPos].exp < poly.termArray[bPos].exp) {
+                c.NewTerm(poly.termArray[bPos].coef, poly.termArray[bPos].exp);
+                bPos++;
+            } else {
                 c.NewTerm(termArray[aPos].coef, termArray[aPos].exp);
                 aPos++;
             }
@@ -81,39 +106,40 @@ public:
             c.NewTerm(termArray[aPos].coef, termArray[aPos].exp);
         }
         for (; bPos < poly.terms; bPos++) {
-            c.NewTerm(termArray[bPos].coef, termArray[bPos].exp);
+            c.NewTerm(poly.termArray[bPos].coef, poly.termArray[bPos].exp);
         }
         
         return c;
     }
     //--------------------------------------------------------------
-    Polynomial Mult(Polynomial poly){
+    Polynomial Mult(Polynomial poly) {
         Polynomial c;
         for (int i = 0; i < terms; i++) {
             Polynomial temp;
             for (int j = 0; j < poly.terms; j++) {
-                temp.NewTerm(termArray[i].coef * poly.termArray[j].coef, termArray[i].exp * poly.termArray[j].exp);
+                float newCoef = termArray[i].coef * poly.termArray[j].coef;
+                int newExp = termArray[i].exp + poly.termArray[j].exp;
+                temp.NewTerm(newCoef, newExp);
             }
-            c.Add(temp);
+            c = c.Add(temp);  // 累加結果
         }
         return c;
     }
     //--------------------------------------------------------------
-    float Eval(float f){
+    float Eval(float f) {
         float ans = 0;
         for (int i = 0; i < terms; i++) {
-            ans = termArray[i].coef * pow(f, termArray[i].exp);
+            ans += termArray[i].coef * pow(f, termArray[i].exp);
         }
         return ans;
     }
-    
     //--------------------------------------------------------------
-    void NewTerm(const float theCoef, const int theExp){
+    void NewTerm(const float theCoef, const int theExp) {
         if (terms == capacity) {
             capacity *= 2;
             Term *temp = new Term[capacity];
             copy(termArray, termArray + terms, temp);
-            delete termArray;
+            delete[] termArray;
             termArray = temp;
         }
         termArray[terms].coef = theCoef;
@@ -122,27 +148,26 @@ public:
     //--------------------------------------------------------------
 };
 
-int main(){
+int main() {
     float coef;
     int exp, terms;
     Polynomial a;
     Polynomial b;
     cin >> terms;
-    cout << "terms in" << endl;
     for(; terms > 0; terms--){
         cin >> coef >> exp;
-        cout << "coef exp in" << endl;
         a.NewTerm(coef, exp);
-        cout << "term in" << endl;
     }
+    a.show();
     cin >> terms;
     for(; terms > 0; terms--){
         cin >> coef >> exp;
         b.NewTerm(coef, exp);
     }
+    b.show();
     cout << "f1() + f2() = ";
-    a.Add(b);
+    (a.Add(b)).show();
     cout << "f1() * f2() = ";
-    a.Mult(b);
+    (a.Mult(b)).show();
     return 0;
 }
